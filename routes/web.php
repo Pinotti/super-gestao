e1c194833b52462094c5c128aba617af5373d04a
Anderson <anderson.pinotti@gmail.com>
2022-07-30 14:40:24 -0300

    Nomeando rota

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\PrincipalController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('principal.index');
