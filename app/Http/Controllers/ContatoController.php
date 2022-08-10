f49230f8a4a0f17eddf94eb51f788c716682ee18
Anderson <anderson.pinotti@gmail.com>
2022-08-10 15:45:42 -0300

    feat: Alteração na forma de acesso aos motivos de contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -14,7 +14,13 @@ class ContatoController extends Controller
      */
     public function contato()
     {
-        return view('site.contato');
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        
+        return view('site.contato', ['motivo_contatos' => $motivo_contatos]);
     }
 
     /**
