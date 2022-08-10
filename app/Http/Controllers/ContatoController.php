d5eddcd5e1598fbb7cb92a9cee20e5821b493102
Anderson <anderson.pinotti@gmail.com>
2022-08-10 15:07:17 -0300

    feat: Inclusão de validação dos campos do formulário

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
@@ -34,7 +35,16 @@ class ContatoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000',
+        ]);
+
+        SiteContato::create($request->all());
     }
 
     /**
